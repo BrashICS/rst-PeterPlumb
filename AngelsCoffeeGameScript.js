b34d52eca477cd35f1c7a15711b9ffca84a5c46d
Peter Blackadar <peter.blackadar@stu.ocsb.ca>
2024-06-17 09:42:34 -0400

    Before changing "1's" to 0

--- AngelsCoffeeGameScript.js
+++ AngelsCoffeeGameScript.js
@@ -1,70 +1,77 @@
 'use strict';
-/**document.getElementById("WhippedCream").addEventListener("DragEvent", userdrink)/*
-/**document.getElementById("UserCupInput").addEventListener("Drop", userdrink)*/
- /**document.getElementById("Layer1ID").addEventListener("Drop", layer)*/
-/**document.getElementById("Layer2ID").addEventListener("Drop", changeImage)*/
-document.getElementById('Checker').addEventListener("onclick", FoodCheck)
-document.getElementById('ResetButton').addEventListener("onclick", Reset)
+document.getElementById('Checker').addEventListener("click", Checker)
+document.getElementById('ResetButton').addEventListener("click", Reset)
+
 document.getElementById('WhippedCream').addEventListener('click', WhippedLayer3Function)
+document.getElementById('ChocaloteChip').addEventListener('click', ChocaloteChipLayer3Function)
+document.getElementById('Cinnamon').addEventListener('click', CinnamonLayer3Function)
 
-if (topping1 == document.getElementById("Whipped")) {
+document.getElementById('Wafer').addEventListener('click', WaferLayer2Function)
+document.getElementById('Nuts').addEventListener('click', NutsLayer2Function)
+document.getElementById('Nutmeg').addEventListener('click', NutmegLayer2Function)
 
-}
-
-/**
-function changeImage(Image) {
-  document.getElementById("Layer1").src
-}
-function layer() {
-  if (document.getElementById == ("Layer1")) {
-    document.getElementById("Layer1").hidden = false
-  }
-  else if (document.getElementById == ("Layer2")) {
-    document.getElementById("Layer2").hidden = false
-  }
-  else if (document.getElementById == ("Layer3")) {
-    document.getElementById("Layer3").hidden = false
-  }
-
-}
-
-
-*/
+document.getElementById('Cream').addEventListener('click', CreamLayer1Function)
+document.getElementById('ColdBrew').addEventListener('click', ColdBrewLayer1Function)
 
 let Score = 0
+let NofGuests = 0
 
-function Layer1Function() {
+function Checker() {
+  if (topping1 == UserInputLayer3 && topping2 == UserInputLayer2 && CoffeeStyle == UserInputLayer1) {
+    Score + 1
+     NofGuests - 1
+      document.getElementById('Guest').hidden = true
+}
+
+else {
+  NofGuests - 1
+  document.getElementById('Guest').hidden = true
+}
 
 }
 
-function Layer2Function() {
-
-}
 
 let UserInputLayer3 = 0
 function WhippedLayer3Function() {
   document.getElementById('Layer3').src= document.getElementById("WhippedCream")
   UserInputLayer3 = WhippedCream1
 }
+function ChocaloteChipLayer3Function() {
+  document.getElementById('Layer3').src= document.getElementById("ChocaloteChip")
+  UserInputLayer3 = ChocaloteChips1
+}
+function CinnamonLayer3Function() {
+  document.getElementById('Layer3').src= document.getElementById("Cinnamon")
+  UserInputLayer3 = Cinnamon1
+}
 
 let UserInputLayer2 = 0
 
+function WaferLayer2Function() {
+  document.getElementById('Layer2').src = "Images/WaferImg.png"
+  UserInputLayer2 = Wafers1
+}
+
+function NutsLayer2Function() {
+  document.getElementById('Layer2').src = "Images/Nuts.png"
+  UserInputLayer2 = Nuts1
+}
+
+function NutmegLayer2Function() {
+  document.getElementById('Layer2').src = "Images/Nutmeg.png"
+  UserInputLayer2 = Nuts1
+}
+
 let UserInputLayer1 = 0
 
-function FoodCheck() {
-/**
-  if { (document.getElementById(Layer1 == CoffeeStyle;)
-  }
-  (document.getElementById(Layer1 == CoffeeStyle, Layer2 == topping1, Layer3 == topping2)){
-    Score = Score + 1
-    NofGuests = NofGuests - 1
-    document.getElementById('Guest').hidden = true
-  }
-  else {
-    NofGuests = NofGuests - 1
-    document.getElementById('Guest').hidden = true
-  }
-  */
+function ColdBrewLayer1Function() {
+  document.getElementById('Layer1').src = "Images/ColdBrew.png"
+  UserInputLayer1 = Coldbrew1
+}
+
+function CreamLayer1Function() {
+  document.getElementById('Layer1').src = "Images/Cream.png"
+  UserInputLayer1 = Cream1
 }
 
 function Reset() {
@@ -94,16 +101,16 @@ function CoffeeType(){;
 }
 
 let WhippedCreamG = document.getElementById("WhippedCream")
-let CinnamonG = document.getElementById("")
-let ChocaloteChipsG = document.getElementById("")
-let WafersG = document.getElementById("")
-let NutsG = document.getElementById("")
-let NutmegG = document.getElementById("")
+let CinnamonG = document.getElementById("Cinnamon")
+let ChocaloteChipG = document.getElementById("ChocaloteChip")
+let WafersG = document.getElementById("Wafer")
+let NutsG = document.getElementById("Nuts")
+let NutmegG = document.getElementById("Nutmeg")
 
 
 let WhippedCream1 = WhippedCreamG
 let Cinnamon1 = CinnamonG
-let ChocaloteChips1 = ChocaloteChipsG
+let ChocaloteChips1 = ChocaloteChipG
 let Wafers1 = WafersG
 let Nuts1 = NutsG
 let Nutmeg1 = NutmegG
@@ -125,10 +132,10 @@ function toppings() {
   return order_of_toppings1
 }
 
-let topping2 = randInt(1, 3)
+
 let order_of_toppings2 = 0
 function toppings2() {
-  let topping2 = randInt(1, 5)
+  let topping2 = randInt(1, 3)
   if (topping2 == 1){;
   order_of_toppings2 = WhippedCream1
   }
@@ -141,20 +148,23 @@ function toppings2() {
   return order_of_toppings2
 }
 
+let topping1 = 0
+let topping3 = 0
+let CoffeeStyle = 0
+let toppings1G = 0
+let toppings2G = 0
+let CoffeeStyleG = 0
 function order(){
-  let topping1 = order_of_toppings1
-  let topping2 = order_of_toppings2
+   topping1 = order_of_toppings1
+   topping3 = order_of_toppings2
   let CoffeeStyle = CoffeeT
-  let GuestsOrder1 = (CoffeeStyle + topping1 + topping2)
+  let GuestsOrder1 = (CoffeeStyle + topping1 + topping3)
   document.getElementById('Guest').hidden = false
   NofGuests = NofGuests + 1
   return GuestsOrder1
 }
 
 
-let toppings1G = 0
-let toppings2G = 0
-let CoffeeStyleG = 0
 function GuestsOrder() {
   if(topping1 == Wafers1){
     toppings1G =="Wafers"
@@ -165,13 +175,13 @@ function GuestsOrder() {
   else if(topping1 == Nutmeg1){
     toppings1G =="Nutmeg"
   }
-  else if(topping2 == WhippedCream1){
+  else if(topping3 == WhippedCream1){
     toppings2G =="Whipped Cream"
   }
-  else if(topping2 == Cinnamon1){
+  else if(topping3 == Cinnamon1){
     toppings2G =="Cinnamon"
   }
-  else if(topping2 == ChocaloteChips1){
+  else if(topping3 == ChocaloteChips1){
     toppings2G =="Chocalote Chips"
   }
   else if(CoffeeStyle == Coldbrew1){
@@ -194,10 +204,15 @@ while (NofGuests == 0){
   order()
 }
 
+console.log(randInt(1,3))
+console.log(order())
 CoffeeType()
 toppings2()
 toppings()
+GuestsOrder()
 console.log(GuestsOrder())
+console.log(order_of_toppings1)
+console.log(order_of_toppings2)
 
 
 
